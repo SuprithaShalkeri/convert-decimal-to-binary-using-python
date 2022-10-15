08c4225f5e6afac00ee079b853d6dee064fc6f70
Supritha26 <115646725+SuprithaShalkeri@users.noreply.github.com>
2022-10-15 11:31:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def decimalToBinary(num):
+  if num>1:
+        decimalToBinary9num // 2
+        print(num % 2, end='')
+
+number = int(input("enter any decimal number:"))
+
+decimalToBinary(number)
